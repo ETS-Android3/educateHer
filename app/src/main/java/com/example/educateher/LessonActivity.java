d03b15ab874d2f586059b8d8fea396d19bc6eb0d
Avinash Vijayvargiya <avinash14022002@gmail.com>
2021-12-31 23:31:37 +0530

    Added: LessonActivity.java

--- app/src/main/java/com/example/educateher/LessonActivity.java
+++ app/src/main/java/com/example/educateher/LessonActivity.java
@@ -1,14 +1,64 @@
 package com.example.educateher;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LessonActivity extends AppCompatActivity {
+public class LessonActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button lessonOneButton;
+    Button lessonTwoButton;
+    Button lessonThreeButton;
+    Button lessonFourButton;
+    Button lessonFiveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
+
+        lessonOneButton = findViewById(R.id.lessonOneButton);
+        lessonOneButton.setOnClickListener(this);
+
+        lessonTwoButton = findViewById(R.id.lessonTwoButton);
+        lessonTwoButton.setOnClickListener(this);
+
+        lessonThreeButton = findViewById(R.id.lessonThreeButton);
+        lessonThreeButton.setOnClickListener(this);
+
+        lessonFourButton = findViewById(R.id.lessonFourButton);
+        lessonFourButton.setOnClickListener(this);
+
+        lessonFiveButton = findViewById(R.id.lessonFiveButton);
+        lessonFiveButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        int module_num = 0;
+        if (id == R.id.lessonOneButton) {
+            module_num = 1;
+        } else if (id == R.id.lessonTwoButton) {
+            module_num = 2;
+        } else if (id == R.id.lessonThreeButton) {
+            module_num = 3;
+        } else if (id == R.id.lessonFourButton) {
+            module_num = 4;
+        } else if (id == R.id.lessonFiveButton) {
+            module_num = 5;
+        }
+        startModule(module_num);
+    }
+
+    public void startModule(int moduleNum) {
+        Intent i = new Intent(this, FullscreenActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt("moduleNum", moduleNum);
+        i.putExtras(bundle);
+        startActivity(i);
     }
 }
